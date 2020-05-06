512ec28d18d70b4ced23ce9e3af5505551b3c96f
Jeff B <jeffydb@gmail.com>
2020-05-06 16:09:50 -0700

    Initial working version, 16x16 squares

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Your import worked");
+
+let resolution = 16;
+
+let canvas = {
+  draw: function() {
+    var canvasContainer = document.getElementById('container');
+    for (i = 0; i < resolution ** 2; i++) {
+      var newPixel = document.createElement('div');
+      newPixel.classList.add('canvasPixels');
+      newPixel.setAttribute('id', i);
+      newPixel.addEventListener('mouseover', function(event) {
+        let pixelHovered = event.target;
+        if (pixelHovered.className === 'canvasPixels') {
+          pixelHovered.classList.remove('canvasPixels');
+          pixelHovered.classList.add('canvasPixelsHovered');
+        }
+      })
+      canvasContainer.appendChild(newPixel);
+    }
+  }
+}
+
+canvas.draw()
