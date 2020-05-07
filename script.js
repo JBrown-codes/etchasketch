2fb261de41e411cc6b59e4898226eac9113ec7d4
BeigePanda <jeffydb@gmail.com>
2020-05-06 22:26:30 -0700

    Working reset button and resolution field; want to add color choice

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.log("Your import worked");
-
 let resolution = 16;
+let colorBackground = 'white';
+let colorForeground = 'black';
 
 let canvas = {
   draw: function() {
     var canvasContainer = document.getElementById('container');
+    canvasContainer.style.gridTemplateColumns=`repeat(${resolution}, 1fr)`;
+    canvasContainer.style.gridTemplateRows=`repeat(${resolution}, 1fr)`;
     for (i = 0; i < resolution ** 2; i++) {
       var newPixel = document.createElement('div');
       newPixel.classList.add('canvasPixels');
@@ -18,7 +20,31 @@ let canvas = {
       })
       canvasContainer.appendChild(newPixel);
     }
+  },
+  reset: function() {
+    var canvasContainer = document.getElementById('container');
+    canvasContainer.innerHTML = "";
+    this.draw();
+  },
+  changeResolution: function(newResolution) {
+    resolution = newResolution;
+    this.reset();
   }
 }
 
+var resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', function() {
+  canvas.reset();
+});
+var resolutionSelector = document.getElementById('resolutionSelector');
+resolutionSelector.addEventListener('change', function() {
+  newResolution = resolutionSelector.value;
+  canvas.changeResolution(newResolution);
+});
+// var backgroundColorSelector = document.getElementById('backgroundColorSelector');
+// backgroundColorSelector.addEventListener('change', function() {
+//   newBackgroundColor = backgroundColorSelector.value;
+//   canvas.changeBackground(newBackgroundColor);
+// })
 canvas.draw()
+// canvas.changeResolution(resolutionSelector.value)
